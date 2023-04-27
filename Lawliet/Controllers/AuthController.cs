2c10e316a05ebbb66446e8974895aa6bdde31ebb
gadzhiev_g <gadzhiev_g@GADZHIDEV>
2023-04-27 14:37:25 +0600

    added default user

--- Lawliet/Controllers/AuthController.cs
+++ Lawliet/Controllers/AuthController.cs
@@ -57,7 +57,7 @@ namespace Lawliet.Controllers {
                 }
             });*/
 
-            user.Id = new Random().Next(0, 100000).ToString();
+            user.Id = "666";
             user.Name = "Гаджиев Гаджи";
             user.PictureUrl = "https://media.istockphoto.com/id/1300845620/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C-icon-flat-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F-%D0%B8%D0%BB%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0.jpg?s=612x612&w=0&k=20&c=Po5TTi0yw6lM7qz6yay5vUbUBy3kAEWrpQmDaUMWnek=";
             user.Email = "gadzhiruslanovich@gmail.com";
@@ -90,7 +90,7 @@ namespace Lawliet.Controllers {
             var isUserInCache = _cachingService.GetObjectFromCache<User>(user.Id);
             if (isUserInCache != null) {
                 HttpContext.Response.Cookies.Append("id", user.Id);
-                await _cachingService.AddObjectFromCache(user);
+                await _cachingService.AddObjectFromCache(isUserInCache);
                 var previousPage = HttpContext.Request?.Cookies["previousPage"]?.Split('/');
                 return (previousPage == null) ? RedirectToAction("Index", "Home") : RedirectToAction(previousPage[1], previousPage[0]);
             }
